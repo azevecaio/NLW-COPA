01922a33ec0dbaf9460df56655413765de4f941e
Caio Martins <azevecaio@gmail.com>
2022-11-05 23:36:45 -0300

    Aula 3

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,18 +4,26 @@ import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@ex
 
 import { SignIn } from './src/screens/SignIn'
 import { Loading } from './src/components/Loading';
+import { New } from './src/screens/New'
+import { Find } from './src/screens/Find'
+import { Pools } from './src/screens/Pools'
 import { THEME } from './src/styles/theme'; 
+
+import { AuthContextProvider } from './src/context/AuthContent';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold})
   return (
+    //Compartilha informação abaixo com toda a aplicação
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor='transparent'
-        translucent
-      />
-        {fontsLoaded ? <SignIn/> : <Loading/>}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor='transparent'
+          translucent
+        />
+          {fontsLoaded ? <Pools/> : <Loading/>}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
---
+++ mobile/src/@types/svg.d.ts
@@ -0,0 +1,7 @@
+//Remove "erro" da importação do svg como Logo, Tipagem do svg.
+declare module "*.svg" {
+  import React from 'react';
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
